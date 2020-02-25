959c987dd923cc36125debb744ea0cfb980e5158
Nicolas Donoso <nicodonosocastex@hotmail.com>
2020-02-25 18:35:59 -0300

    work in progress, test for show

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,7 +1,8 @@
 class DoctorsController < ApplicationController
+skip_before_action :authenticate_user!, only: :index
 
-def index
-end
 
+  def index
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'doctors', to: 'doctors#index', as: 'doctors'
+  get "test", to: 'pages#test'
 end
