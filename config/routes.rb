84d17b8d66f8d2fe75f73d526f81bfd7775db7b8
Maximo Chalbaud <maxichalbaud@gmail.com>
2020-02-25 15:13:34 -0300

    users can create appointments with current user id and with Date only, patient_id still has to be added manually

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'doctors', to: 'doctors#index', as: 'doctors'
+  resources :doctors, only: [:index, :show] do
+  resources :appointments, only: [:new, :create]
+  end
 end
