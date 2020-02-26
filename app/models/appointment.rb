0d759c600644aac711def85876a09ef63d756635
Maximo Chalbaud <maxichalbaud@gmail.com>
2020-02-26 16:17:11 -0300

    done

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,7 +1,7 @@
 class Appointment < ApplicationRecord
   belongs_to :doctor, class_name: "User", foreign_key: "doctor_id"
   belongs_to :patient, class_name: "User", foreign_key: "patient_id"
-  has_many :reviews, class_name: "Review", foreign_key: "review_id"
+  has_one :review
   validates :doctor_id, presence: true
   validates :patient_id, presence: true
   validates :appointment_date, presence: true
