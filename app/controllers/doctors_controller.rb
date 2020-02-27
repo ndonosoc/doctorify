41308c7ceb810bff23c628e9db9b875a5beee898
Analida Corro <analidacorro@gmail.com>
2020-02-27 10:49:58 -0300

    search

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -3,6 +3,15 @@ class DoctorsController < ApplicationController
 
   def index
     @doctors = policy_scope(User).order(created_at: :desc)
+    if params[:specialization].present?
+      sql_query = "specialization ILIKE :specialization"
+      @doctors = User.where(sql_query, specialization: "%#{params[:specialization]}%")
+    else
+      @doctors = User.all
+    end
+    # if params[:location].present?
+      # @doctors.near(params[:location])
+    # end
   end
 
 end
